6d220300da947708e2895ebd173323e8e6875892
dro14 <doniyorbek752@gmail.com>
2025-06-12 10:18:33 +0500

    remove authentication

--- handler/handler.go
+++ handler/handler.go
@@ -1,33 +1,23 @@
 package handler
 
 import (
-	"log"
-	"os"
-
 	"github.com/dro14/yordamchi-api/data"
 	"github.com/dro14/yordamchi-api/provider"
 	"github.com/gin-gonic/gin"
 )
 
 type Handler struct {
-	apiKey   string
 	engine   *gin.Engine
 	data     *data.Data
 	provider *provider.Provider
 }
 
 func New() *Handler {
-	apiKey, ok := os.LookupEnv("API_KEY")
-	if !ok {
-		log.Fatal("api key is not specified")
-	}
-
 	engine := gin.New()
 	engine.Use(gin.Logger())
 	engine.Use(gin.CustomRecovery(notifyOnPanic))
 
 	return &Handler{
-		apiKey:   apiKey,
 		engine:   engine,
 		data:     data.New(),
 		provider: provider.New(),
--- handler/handler_methods.go
+++ handler/handler_methods.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/dro14/yordamchi-api/utils/e"
 	"github.com/dro14/yordamchi-api/utils/info"
 	"github.com/gin-gonic/gin"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
@@ -14,23 +13,20 @@ func (h *Handler) Run(port string) error {
 	h.engine.SetTrustedProxies([]string{"127.0.0.1"})
 
 	h.engine.GET("", h.root)
-	h.engine.POST("/info/bot", h.info)
+	h.engine.POST("/info", h.info)
 
-	authorized := h.engine.Group("")
-	authorized.Use(h.authMiddleware)
-
-	group := authorized.Group("/user")
+	group := h.engine.Group("/user")
 	group.POST("", h.createUser)
 
-	group = authorized.Group("/chat")
+	group = h.engine.Group("/chat")
 	group.POST("", h.createChat)
 	group.DELETE("", h.deleteChats)
 
-	group = authorized.Group("/message")
+	group = h.engine.Group("/message")
 	group.POST("", h.createMessage)
 	group.PUT("", h.editMessage)
 
-	group = authorized.Group("/image")
+	group = h.engine.Group("/image")
 	group.POST("", h.createImage)
 
 	return h.engine.Run(":" + port)
@@ -50,19 +46,4 @@ func (h *Handler) info(c *gin.Context) {
 	}
 	info.Update(update)
 	c.JSON(http.StatusOK, gin.H{"ok": true})
-}
-
-func (h *Handler) authMiddleware(c *gin.Context) {
-	apiKey := c.GetHeader("identifikatsiya")
-	if apiKey == "" {
-		c.JSON(http.StatusUnauthorized, failure(e.ErrNoIdHeader))
-		c.Abort()
-		return
-	}
-
-	if apiKey != h.apiKey {
-		c.JSON(http.StatusUnauthorized, failure(e.ErrNoIdHeader))
-		c.Abort()
-		return
-	}
 }
